173b909de622430a07cdf4625220b65aab478bb4
Jiyo <ji.yyou@gmail.com>
2020-07-24 18:18:01 -0400

    Event setup

--- server/server.js
+++ server/server.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
-app.use('/api', graphqlHttp({
+app.use('/api', graphqlHTTP({
 	schema: buildSchema(`
+		type Event {
+			_id: ID!
+			title: String!
+			description: String!
+			price: Float!
+			date: String!
+		}
+
+		input EventInput {
+			title: String!
+			description: String!
+			price: Float!
+			date: String!
+		}
+
 		type RootQuery {
-			events: [String!]!
+			events: [Event!]!
 		}
 
 		type RootMutation {
-			createEvent(name: String): String
+			createEvent(eventInput: EventInput): Event
 		}
 		
 		schema {
@@ -24,13 +41,20 @@ app.use('/api', graphqlHttp({
 	`),
 	rootValue: {
 		events: () => {
-			return ['Romantic Cooking', 'Sailing', 'All-night Coding'];
+			return events;
 		},
 		createEvent: (args) => {
-			const eventName = args.name;
-			return eventName;
+			const event = {
+				_id: Math.random().toString(),
+				title: args.title,
+				description: args.description,
+				price: +args.price,
+				date: new Date().toISOString()
+			};
+			events.push(event);
 		}
-	}
+	},
+	graphiql: true
 }));
 
 app.listen(8080);
